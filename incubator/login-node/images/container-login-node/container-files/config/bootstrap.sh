67b5d960d1250b2858c6c8cd9062543a5f1591c0
Lincoln Bryant <lincolnb@uchicago.edu>
2019-10-11 15:26:32 -0500

    cleaned up some old comments

--- incubator/login-node/images/container-login-node/container-files/config/bootstrap.sh
+++ incubator/login-node/images/container-login-node/container-files/config/bootstrap.sh
@@ -21,9 +21,7 @@ ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' \
 # Add new users
 if [ -n "$PASSWDFILE" ]; then
   echo '$PASSWDFILE is set:' $PASSWDFILE
-#  chmod +x /Users/nehalingareddy/neha_test/python_api/python/new/wookiee2187/container-files/config/init/new.sh
   bash config/init/new.sh $PASSWDFILE
-#/Users/nehalingareddy/neha_test/python_api/python/new/wookiee2187/container-files/config/init/new.sh $PASSWDFILE
 else
   echo '$PASSWDFILE not defined! Not creating users'
 fi
