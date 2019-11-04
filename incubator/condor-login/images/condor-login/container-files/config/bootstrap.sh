618e4e37ac83763e3f73e691b7aff998cd9652e1
Mitchell <35324660+Mansalu@users.noreply.github.com>
2019-11-04 10:47:12 -0700

    add kerberos configuration

--- incubator/condor-login/images/condor-login/container-files/config/bootstrap.sh
+++ incubator/condor-login/images/condor-login/container-files/config/bootstrap.sh
@@ -26,4 +26,22 @@ else
   echo '$PASSWDFILE not defined! Not creating users'
 fi
 
+if [ -z "${KRBREALM+x}" ]; then
+  echo 'Kerberos configuration is empty'
+  echo 'Nothing to do'
+else
+  echo 'Kerberos configuration detected'
+  echo 'Checking for KDC'
+  if [ -z "${KRBKDC+x}" ]; then
+    echo 'KDC configuration appears to be empty'
+    echo 'Nothing to do'
+  else
+    echo "Attempting to use authconfig to enable Kerberos login at $KRBREALM"
+    authconfig --enablekrb5 --krb5adminserver="" --krb5realm=$KRBREALM \
+    --krb5kdc=$KRBKDC \
+    --disablekrb5kdcdns --disablekrb5realmdns --enablelocauthorize --enablepamaccess \
+    --disablenis --enablesssd --enablesssdauth --disablefingerprint --disablesmartcard --update
+  fi
+fi 
+
 supervisord -n $SUPERVISOR_PARAMS
