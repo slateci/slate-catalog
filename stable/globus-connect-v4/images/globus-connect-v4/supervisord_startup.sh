75dfd67129ad1a0450bc86f0d3d2a8db4a7f2697
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2020-04-08 13:50:03 -0500

    reverted some fanciness, added some quoting to please the shellcheck gods

--- stable/globus-connect-v4/images/globus-connect-v4/supervisord_startup.sh
+++ stable/globus-connect-v4/images/globus-connect-v4/supervisord_startup.sh
@@ -34,12 +34,12 @@ fi
 # check for a globus_server defined as an environment variable. if we get one,
 # check to see if it has a port attached. if there is, comment out the default
 # port so globus can do whatever with its gridftp.d file
-if [ -z ${GLOBUS_SERVER+x} ]; then
+if [ -z "$GLOBUS_SERVER" ]; then
   echo "GLOBUS_SERVER is undefined"
 else
-  GLOBUS_PORT=$(echo $GLOBUS_SERVER | cut -d':' -f2 -s)
+  GLOBUS_PORT=$(echo "$GLOBUS_SERVER" | cut -d':' -f2 -s)
   # if we actually get a port out of that, then comment out the gridftp defualt
-  if [ -z ${GLOBUS_PORT} ]; then
+  if [ -z "$GLOBUS_PORT" ]; then
     echo "Globus port is an empty string, assume defaults"
   else
     sed -i 's/port/#port/' /etc/gridftp.conf
