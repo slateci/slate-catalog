7c4a638e97ad785c31730468da4a60c7e626dc60
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2020-05-04 14:16:14 -0500

    Added the GridFTP image to the catalog

---
+++ stable/gridftp/images/gridftp/image-config/20-certificate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# need to copy the certificate and key into the right place and chmod them
+stat /root/gridftp-certificates
+if [ $? -eq 0 ]; then 
+  echo "Found certificate directory, trying to copy the files out"
+  cp /root/gridftp-certificates/hostcert.pem \
+    /root/gridftp-certificates/hostkey.pem  /etc/grid-security
+  if [ $? -ne 0 ]; then 
+    echo "Couldn't copy the certificates into place. Are you sure they're called hostcert and hostkey?"
+  else
+    echo "Setting proper mode on the certificate and key"
+    chmod 600 /etc/grid-security/hostcert.pem
+    chmod 400 /etc/grid-security/hostkey.pem
+  fi
+else 
+  echo "Couldn't find a certificate directory. Doing nothing."
+fi
