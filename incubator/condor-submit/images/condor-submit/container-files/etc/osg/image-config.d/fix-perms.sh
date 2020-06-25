5d734e14730689fa7ae015408cf54e45acc0eecf
cnweaver <cnweaver@gmail.com>
2020-06-25 12:30:21 -0500

    Fix credential permissions problems

--- incubator/condor-submit/images/condor-submit/container-files/etc/osg/image-config.d/fix-perms.sh
+++ incubator/condor-submit/images/condor-submit/container-files/etc/osg/image-config.d/fix-perms.sh
@@ -1,3 +1,6 @@
 cp -a /root/tokens/* /etc/condor/tokens.d/
 chown -R condor: /etc/condor/tokens.d
-chmod 644 /etc/condor/tokens.d/*
+chmod 400 /etc/condor/tokens.d/*
+cp /root/condor_password /etc/condor/password
+chown condor: /etc/condor/password
+chmod 400 /etc/condor/password
