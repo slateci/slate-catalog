14625764eff395c1530fa63bb71d09fbc6d24985
cnweaver <cnweaver@gmail.com>
2020-06-25 12:42:45 -0500

    Condor requires its password to be owned by root, actually

--- incubator/condor-submit/images/condor-submit/container-files/etc/osg/image-config.d/fix-perms.sh
+++ incubator/condor-submit/images/condor-submit/container-files/etc/osg/image-config.d/fix-perms.sh
@@ -1,6 +1,3 @@
 cp -a /root/tokens/* /etc/condor/tokens.d/
 chown -R condor: /etc/condor/tokens.d
 chmod 400 /etc/condor/tokens.d/*
-cp /root/condor_password /etc/condor/password
-chown condor: /etc/condor/password
-chmod 400 /etc/condor/password
