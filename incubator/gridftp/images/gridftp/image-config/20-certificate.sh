bceb531761fc11f00d9b3dda94bbc5de7b1555b1
Soundar Rajendran <rajends@rajends-mbp.attlocal.net>
2020-11-23 16:08:05 -0500

    Changed x509 cert config in gridftp to match xcache app

--- incubator/gridftp/images/gridftp/image-config/20-certificate.sh
+++ incubator/gridftp/images/gridftp/image-config/20-certificate.sh
@@ -4,14 +4,14 @@
 stat /root/gridftp-certificates
 if [ $? -eq 0 ]; then 
   echo "Found certificate directory, trying to copy the files out"
-  cp /root/gridftp-certificates/hostcert.pem \
-    /root/gridftp-certificates/hostkey.pem  /etc/grid-security
+  cp /root/gridftp-certificates/usercert.pem \
+    /root/gridftp-certificates/userkey.pem  /etc/grid-certs/
   if [ $? -ne 0 ]; then 
     echo "Couldn't copy the certificates into place. Are you sure they're called hostcert and hostkey?"
   else
     echo "Setting proper mode on the certificate and key"
-    chmod 600 /etc/grid-security/hostcert.pem
-    chmod 400 /etc/grid-security/hostkey.pem
+    chmod 600 /etc/grid-certs/usercert.pem
+    chmod 400 /etc/grid-certs/userkey.pem
   fi
 else 
   echo "Couldn't find a certificate directory. Doing nothing."
