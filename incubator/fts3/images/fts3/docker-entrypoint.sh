9b046da4f937c54c333002487eaaedcd017ea089
Li Zhang <lizhang@Lis-MacBook-Pro.local>
2020-12-09 11:02:16 -0600

    uncommented line 9 and 11 in docker-entrypoint.sh

--- incubator/fts3/images/fts3/docker-entrypoint.sh
+++ incubator/fts3/images/fts3/docker-entrypoint.sh
@@ -6,9 +6,9 @@ chown root:root /etc/grid-security/hostcert.pem
 cp /tmp/fts3-host-pems/hostkey.pem /etc/grid-security/
 chmod 400 /etc/grid-security/hostkey.pem
 chown root:root /etc/grid-security/hostkey.pem
-# cp /tmp/fts3-configs/ca.crt /etc/grid-security/certificates/docker-entrypoint_ca.crt
+cp /tmp/fts3-configs/ca.crt /etc/grid-security/certificates/docker-entrypoint_ca.crt
 # cp /tmp/fts3-configs/fts3config /etc/fts3/fts3config
-# cp /tmp/fts3-configs/fts-msg-monitoring.conf /etc/fts3/fts-msg-monitoring.conf
+cp /tmp/fts3-configs/fts-msg-monitoring.conf /etc/fts3/fts-msg-monitoring.conf
 mkdir -p /var/lib/mysql/
 mkdir -p /var/log/fts3rest/ && touch /var/log/fts3rest/fts3rest.log
 chown fts3:fts3 /var/log/
