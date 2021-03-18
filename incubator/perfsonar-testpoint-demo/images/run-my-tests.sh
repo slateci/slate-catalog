18a4caf7582c66ab6d3187054e58f30b5c87d091
Muhammad A <muali@umich.edu>
2021-03-17 23:44:27 -0400

    Added fqdn error handeling

--- incubator/perfsonar-testpoint-demo/images/run-my-tests.sh
+++ incubator/perfsonar-testpoint-demo/images/run-my-tests.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+is_valid_fqdn() {
+pattern='(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{0,62}[a-zA-Z0-9]\.)+[a-zA-Z]{2,63}$)'
+#echo "checking $1 now"
+r=$( echo "$1" | grep -P $pattern)
+
+if [ "$r" = "$1" ]; then
+  return 0 # true  
+else
+  return 1 # false  
+fi
+
+}
+
 run_test(){
+
 echo "======= Running '$1' =======" | tee -a /var/log/demo/demo.out /proc/1/fd/1
 $1 | tee -a  /var/log/demo/demo.out /proc/1/fd/1
 echo -e "\n" | tee -a /var/log/demo/demo.out /proc/1/fd/1
@@ -8,12 +22,19 @@ echo -e "\n" | tee -a /var/log/demo/demo.out /proc/1/fd/1
 
 run_all_tests_to() {
 
-#run_test "pscheduler troubleshoot"
-run_test "pscheduler task throughput -t 30 --dest $1"
-run_test "pscheduler task latency --packet-count 6000 --packet-interval .01 --dest $1"
-run_test "pscheduler task --tool tracepath trace --dest $1"
+if is_valid_fqdn "$1"; then 
+	
+        run_test "pscheduler task throughput -t 30 --dest $1"
+	run_test "pscheduler task latency --packet-count 6000 --packet-interval .01 --dest $1"
+	run_test "pscheduler task --tool tracepath trace --dest $1"
+else 
+
+	echo "Error: '$1' is not a valid FQDN" | tee -a /var/log/demo/demo.out /proc/1/fd/1 
+
+fi
 
 }
+
 
 #Waiting for a limited time for services to start up and be ready
 sleep 20
@@ -23,7 +44,7 @@ while [ $? -ne 0 ] && [ $i -le 4 ]
 do
 echo "Local perfSONAR services are not ready yet...waiting for 15 secs before checking again...." | tee -a /var/log/demo/demo.out /proc/1/fd/1
 sleep 15
-#to limit loop iterations to 4 times
+#Limiting loop iterations to 4 times
 i=$(( $i + 1 ))
 #echo "Checking the status of local perfSONAR services..."
 pscheduler troubleshoot
