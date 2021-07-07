1fdc1a16971de4fd5a7e5b710e126ba9da9e036d
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-07-07 11:07:29 -0600

    Update startup.sh

--- incubator/open-ondemand/images/open-ondemand/startup.sh
+++ incubator/open-ondemand/images/open-ondemand/startup.sh
@@ -7,27 +7,26 @@ OOD_SSHHOST_ALLOWLIST=""
 OOD_SHELL_ORIGIN_CHECK="off"
 EOF
 sleep 30
-if [[ ! -f /opt/rh/httpd24/root/etc/httpd/conf.d/auth_openidc.conf ]]
-then
-    printf "OIDCProviderMetadataURL https://$(echo $SLATE_INSTANCE_NAME).keycloak.$(echo $SLATE_CLUSTER_NAME)/auth/realms/ondemand/.well-known/openid-configuration\n\
-OIDCClientID        \"`cat /shared/id`\" \n\
-OIDCClientSecret    \"`cat /shared/client-secret`\"\n\
-OIDCRedirectURI      https://$(echo $SLATE_INSTANCE_NAME).ondemand.$(echo $SLATE_CLUSTER_NAME)/oidc\n\
-OIDCCryptoPassphrase 'd14e5b4c8e6257ea81830f23c2be4633ad04d3af9816affdda6d8fec8ea926a000ca47b507587dc5'\n\
-\n\
-$(echo '# Keep sessions alive for 8 hours')\n\
-OIDCSessionInactivityTimeout 28800\n\
-OIDCSessionMaxDuration 28800\n\
-\n\
-$(echo '# Set REMOTE_USER')\n\
-OIDCRemoteUserClaim preferred_username\n\
-\n\
-$(echo '# Do not pass claims to backend servers')\n\
-OIDCPassClaimsAs environment\n\
-\n\
-$(echo '# Strip out session cookies before passing to backend')\n\
-OIDCStripCookies mod_auth_openidc_session mod_auth_openidc_session_chunks mod_auth_openidc_session_0 mod_auth_openidc_session_1 " > '/opt/rh/httpd24/root/etc/httpd/conf.d/auth_openidc.conf'
-fi
+cat <<EOF > /opt/rh/httpd24/root/etc/httpd/conf.d/auth_openidc.conf
+OIDCProviderMetadataURL https://$(echo $SLATE_INSTANCE_NAME).keycloak.$(echo $SLATE_CLUSTER_NAME)/auth/realms/ondemand/.well-known/openid-configuration
+OIDCClientID        "`cat /shared/id`" 
+OIDCClientSecret    "`cat /shared/client-secret`"
+OIDCRedirectURI      https://$(echo $SLATE_INSTANCE_NAME).ondemand.$(echo $SLATE_CLUSTER_NAME)/oidc
+OIDCCryptoPassphrase 'd14e5b4c8e6257ea81830f23c2be4633ad04d3af9816affdda6d8fec8ea926a000ca47b507587dc5'
+
+$(echo '# Keep sessions alive for 8 hours')
+OIDCSessionInactivityTimeout 28800
+OIDCSessionMaxDuration 28800
+
+$(echo '# Set REMOTE_USER')
+OIDCRemoteUserClaim preferred_username
+
+$(echo '# Do not pass claims to backend servers')
+OIDCPassClaimsAs environment
+
+$(echo '# Strip out session cookies before passing to backend')
+OIDCStripCookies mod_auth_openidc_session mod_auth_openidc_session_chunks mod_auth_openidc_session_0 mod_auth_openidc_session_1
+EOF
 # Configure apache
 chgrp apache /opt/rh/httpd24/root/etc/httpd/conf.d/auth_openidc.conf
 chmod 640 /opt/rh/httpd24/root/etc/httpd/conf.d/auth_openidc.conf
